5ee288db5c90f8d9858ce055735e1a22a9b6d090
Dennis <denjand91@gmail.com>
2022-06-26 05:22:25 -0400

    finished assigned tests

---
+++ routes/messages.js
@@ -0,0 +1,67 @@
+const Router = require("express").Router;
+const router = new Router()
+const Message = require("../models/message");
+const {ensureLoggedIn, ensureCorrectUser} =require("../middleware/auth");
+const ExpressError = require("../expressError");
+/** GET /:id - get detail of message.
+ *
+ * => {message: {id,
+ *               body,
+ *               sent_at,
+ *               read_at,
+ *               from_user: {username, first_name, last_name, phone},
+ *               to_user: {username, first_name, last_name, phone}}
+ *
+ * Make sure that the currently-logged-in users is either the to or from user.
+ *
+ **/
+ router.get("/:id", async function (req, res, next) {
+    try {
+      let msg = await Message.get(req.params.id)
+      if (req.user.username !== msg.from_user.username && req.user.username !== msg.to_user.username){
+        throw new ExpressError("Cannot read this message", 401)
+      }
+      return res.json({msg})
+    } 
+    catch (err) {
+      return next(err);
+    }
+  });
+
+
+/** POST / - post message.
+ *
+ * {to_username, body} =>
+ *   {message: {id, from_username, to_username, body, sent_at}}
+ *
+ **/
+router.post("/", ensureLoggedIn ,async function (req, res, next) {
+  try {
+    let msg = Message.create(req.user.username, req.body.to_username, body)
+
+    return res.json({ msg});
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** POST/:id/read - mark message as read:
+ *
+ *  => {message: {id, read_at}}
+ *
+ * Make sure that the only the intended recipient can mark as read.
+ *
+ **/
+ router.post("/", ensureLoggedIn ,async function (req, res, next) {
+    try {
+        let msg = await Message.get(req.params.id)
+        if(req.user.username !== msg.to_user.username){
+            throw new ExpressError("Cannot mark message as read", 401)
+        }
+        let read = Message.markRead(msg.id)
+      return res.json({ read});
+    } catch (err) {
+      return next(err);
+    }
+  });
+
